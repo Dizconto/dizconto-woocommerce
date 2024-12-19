8c1a1c487380c2b251011a3e4584d18a876b5384
MrLightful <romatallinn@gmail.com>
2024-12-19 12:43:49 +0100

    Fix styling for credit card form

--- src/blocks/credit-card-checkout.js
+++ src/blocks/credit-card-checkout.js
@@ -13,44 +13,46 @@ const dizcontoCreditCardPaymentMethod = (() => {
 
     const Content = () => {
         return (
-            <form>
-                <div className="wc-block-components-text-input">
-                    <input
-                        type="text"
-                        id="card-holder-name"
-                        className="input-text"
-                        required
-                    />
-                    <label htmlFor="card-holder-name">Cardholder Name</label>
-                </div>
-                <div className="wc-block-components-text-input">
-                    <label htmlFor="card-number">Card Number</label>
-                    <input
-                        type="text"
-                        id="card-number"
-                        className="input-text"
-                        required
-                    />
-                </div>
-                <div className="wc-block-components-text-input form-row-first">
-                    <label htmlFor="expiry-date">Expiry Date (MM/YY)</label>
-                    <input
-                        type="text"
-                        id="expiry-date"
-                        className="input-text"
-                        required
-                    />
-                </div>
-                <div className="wc-block-components-text-input form-row-last">
-                    <label htmlFor="cvv">CVV</label>
-                    <input
-                        type="text"
-                        id="cvv"
-                        className="input-text"
-                        required
-                    />
-                </div>
-            </form>
+            <div style={{ display: 'flex', paddingBottom: 30 }}>
+                <form style={{ flex: 1 }}>
+                    <div className="wc-block-components-text-input">
+                        <input
+                            type="text"
+                            id="card-holder-name"
+                            className="input-text"
+                            required
+                        />
+                        <label htmlFor="card-holder-name">Cardholder Name</label>
+                    </div>
+                    <div className="wc-block-components-text-input">
+                        <label htmlFor="card-number">Card Number</label>
+                        <input
+                            type="text"
+                            id="card-number"
+                            className="input-text"
+                            required
+                        />
+                    </div>
+                    <div className="wc-block-components-text-input form-row-first">
+                        <label htmlFor="expiry-date">Expiry Date (MM/YY)</label>
+                        <input
+                            type="text"
+                            id="expiry-date"
+                            className="input-text"
+                            required
+                        />
+                    </div>
+                    <div className="wc-block-components-text-input form-row-last">
+                        <label htmlFor="cvv">CVV</label>
+                        <input
+                            type="text"
+                            id="cvv"
+                            className="input-text"
+                            required
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 
