9ed874b6b8be92722c1b21d864aae9ce9b89c6cc
MrLightful <romatallinn@gmail.com>
2024-12-19 01:05:31 +0100

    Add plugin update checker

--- dizconto-wordpress.php
+++ dizconto-wordpress.php
@@ -16,8 +16,18 @@ define( 'DIZCONTO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DIZCONTO_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'DIZCONTO_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-// Include the loader.
-require_once DIZCONTO_PLUGIN_DIR . 'includes/setup.php';
+// Plugin update checker.
+require DIZCONTO_PLUGIN_DIR . 'includes/plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/dizconto/dizconto-wordpress/',
+    __FILE__,
+    'dizconto-wordpress'
+);
+$updateChecker->setBranch('main');
 
 // Initialize the plugin.
+require_once DIZCONTO_PLUGIN_DIR . 'includes/setup.php';
 Dizconto_Loader::init();
+
+
