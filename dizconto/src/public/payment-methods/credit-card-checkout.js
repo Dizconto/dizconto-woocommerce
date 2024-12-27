4d2cd582e2bc422fe6801d6a0335a12386b5fa1d
MrLightful <romatallinn@gmail.com>
2024-12-27 15:22:13 +0100

    Better styling for block credit card checkout form

--- dizconto/src/public/payment-methods/credit-card-checkout.js
+++ dizconto/src/public/payment-methods/credit-card-checkout.js
@@ -1,6 +1,7 @@
 import { decodeEntities } from '@wordpress/html-entities';
 import { getSetting } from '@woocommerce/settings';
 import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { TextInput } from '@woocommerce/blocks-components';
 
 const dizcontoCreditCardPaymentMethod = (() => {
     const paymentMethod = 'dizconto_pay_credit_card';
@@ -18,48 +19,12 @@ const dizcontoCreditCardPaymentMethod = (() => {
 
     const Content = () => {
         return (
-            <div style={{ display: 'flex', paddingBottom: 30 }}>
-                <form style={{ flex: 1 }}>
-                    <div className="wc-block-components-text-input">
-                        <input
-                            type="text"
-                            autoComplete="cc-name"
-                            id="card-holder-name"
-                            className="input-text"
-                            required
-                        />
-                        <label htmlFor="card-holder-name">Cardholder Name</label>
-                    </div>
-                    <div className="wc-block-components-text-input">
-                        <label htmlFor="card-number">Card Number</label>
-                        <input
-                            type="text"
-                            autoComplete="cc-number"
-                            id="card-number"
-                            className="input-text"
-                            required
-                        />
-                    </div>
-                    <div className="wc-block-components-text-input form-row-first">
-                        <label htmlFor="expiry-date">Expiry Date (MM/YY)</label>
-                        <input
-                            type="text"
-                            autoComplete="cc-exp"
-                            id="expiry-date"
-                            className="input-text"
-                            required
-                        />
-                    </div>
-                    <div className="wc-block-components-text-input form-row-last">
-                        <label htmlFor="cvv">CVV</label>
-                        <input
-                            type="text"
-                            autoComplete="cc-csc"
-                            id="cvv"
-                            className="input-text"
-                            required
-                        />
-                    </div>
+            <div className="diz-flex diz-pb-8">
+                <form className="wc-block-components-form diz-flex-1">
+                    <TextInput id="card-holder-name" label="Cardholder Name" type="text" autoComplete="cc-name" required  />
+                    <TextInput id="card-number" label="Card Number" type="text" autoComplete="cc-number" required  />
+                    <TextInput id="card-expiry-date" label="Expiry Date" type="text" autoComplete="cc-exp" className="form-row-first" required  />
+                    <TextInput id="card-cvv" label="CVV" type="text" autoComplete="cc-csc" className="form-row-last" required  />
                 </form>
             </div>
         )
